951531083c5254183bb6e86d2983e00d10954f1c
Daniel Jaffe <danielj@othentic.xyz>
2024-07-23 21:15:39 +0300

    bootstrap fixes

--- ansible/roles/edge/templates/bootstrap.sh
+++ ansible/roles/edge/templates/bootstrap.sh
@@ -34,7 +34,6 @@ main() {
     polygon-edge genesis \
                  --consensus polybft \
                  --chain-id {{ chain_id }} \
-                 --proxy-contracts-admin {{ rootchain_coinbase_address }} \
                  --reward-wallet {{ rootchain_coinbase_address }} \
                  {% for item in hostvars %}{% if (hostvars[item].tags.Role == "fullnode" or hostvars[item].tags.Role == "validator") %} --bootnode /dns4/{{ hostvars[item].tags["Name"] }}/tcp/{{ edge_p2p_port }}/p2p/$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].node_id') {% endif %}{% endfor %} \
                  {% for item in hostvars %}{% if (hostvars[item].tags.Role == "fullnode" or hostvars[item].tags.Role == "validator") %} --premine $(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].address'):1000000000000000000000000 {% endif %}{% endfor %} \
@@ -52,7 +51,6 @@ main() {
 
     polygon-edge polybft stake-manager-deploy \
         --jsonrpc {{ rootchain_json_rpc }} \
-        --proxy-contracts-admin  {{ rootchain_coinbase_address }} \
         --private-key $(cat rootchain-wallet.json | jq -r '.HexPrivateKey') \
         --stake-token {{ stake_token_address }}
 
